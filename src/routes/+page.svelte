b8113a8b707cc92680ee349ee07b999ea159523d
11reed <reed11tim@gmail.com>
2023-08-23 15:59:39 -0400

    added /sent route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -201,6 +201,11 @@
       <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-300 text-xl">Interested in our services? Have a technical question? Reach out to us.</p>
       <form action="https://submit-form.com/7eWAXhnd" class="space-y-8">
           <div>
+            <input
+              type="hidden"
+              name="_redirect"
+              value="https://coffeecodestudio.com/sent"
+              />
               <label for="email" class="block mb-2 text-md font-medium text-gray-900 dark:text-gray-300">Your email</label>
               <input type="email" id="email" name="name" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-700 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="email@gmail.com" required>
           </div>
