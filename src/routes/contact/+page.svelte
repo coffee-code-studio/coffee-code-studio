753d6e937c61c98039bcb211e65a18eec2c6d9f1
11reed <reed11tim@gmail.com>
2023-08-23 15:38:59 -0400

    form update

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -61,18 +61,18 @@
 
                   <form action="https://submit-form.com/7eWAXhnd" class="mt-6">
                       <div class="flex-1">
-                          <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
-                          <input type="text" placeholder="Your name" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                          <label for="name" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
+                          <input type="text" name="name" placeholder="Your name" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                       </div>
 
                       <div class="flex-1 mt-6">
-                          <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
-                          <input type="email" placeholder="Your email" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                          <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
+                          <input type="email" name="name" placeholder="Your email" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                       </div>
 
                       <div class="w-full mt-6">
-                          <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
-                          <textarea class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
+                          <label for="message" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
+                          <textarea name="message" class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
                       </div>
 
                       <button type="submit" class="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
